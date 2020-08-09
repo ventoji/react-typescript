0deafd17263c668dfb9a684a71c037b0b1a88103
Jorge Isaac Vento Maldonado <39380157+ventoji@users.noreply.github.com>
2020-08-09 23:57:00 +0200

    Createing a Reducer with typescript

--- src/ProductsData.ts
+++ src/ProductsData.ts
@@ -70,3 +70,8 @@ export const getProduct = async (id: number): Promise<IProduct | null> => {
 const wait = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
+
+export const getProducts = async (): Promise<IProduct[]> => {
+  await wait(1000);
+  return products;
+};
